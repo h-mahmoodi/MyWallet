ddcba01959cc92ffd99857580a21e2a9e801d099
h-mahmoodi <hesamahmoodi@gmail.com>
2024-12-06 02:34:18 +0100

    Revert c

--- src/pages/app/HomePage/HomePage.tsx
+++
@@ -1,6 +0,0 @@
-import styles from "./home-page.module.css";
-
-const HomePage = () => {
-  return <div className={styles.hero}></div>;
-};
-export default HomePage;
---
+++ src/pages/app/home-page/index.tsx
@@ -0,0 +1,6 @@
+import styles from "./styles.module.css";
+
+const AppHomePage = () => {
+  return <div className={styles.hero}></div>;
+};
+export default AppHomePage;
